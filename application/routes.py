940bd697c8e13bd7851c126554a5a4bd64163129
Rj12-art <rashj40@gmail.com>
2023-01-30 19:21:57 +0000

    made some changes on the rout file

--- application/routes.py
+++ application/routes.py
@@ -62,8 +62,3 @@ def dashboard():
                             dates_label =json.dumps(dates_label)
                         )
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get(
-        "IP"), port=int(
-            os.environ.get(
-                "PORT")), debug=False)
